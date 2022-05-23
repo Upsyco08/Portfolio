304b5d44add78826da193245bcc96d40c5af383e
Sandesh Raj Shakya <sandsshakya444@gmail.com>
2022-05-23 17:18:59 +0545

    Changes to portfolio and added portfolioList along with data.js

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,7 +1,26 @@
 import './Intro.scss'
-import profilePicture from "../../Images/img3.jpg"
+import profilePicture from "../../Images/img1_edited.png"
+import arrow from "../../Images/arrow.png"
+import { useEffect, useRef } from 'react'
+import { init } from 'ityped';
+
+let isLoading=true;
 
 function Intro() {
+  const textRef= useRef();
+  useEffect(()=>{
+    if (textRef.current && isLoading){
+      isLoading =false;
+    
+    init(textRef.current,{
+      showCursor: true,
+      backDealy: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Designer"]
+    });
+    }
+  }, []);
+
   return (
     <div className='intro' id="intro">
       <div className='left'>
@@ -10,7 +29,14 @@ function Intro() {
         </div>
       </div>
       <div className='right'>
-
+        <div className='wrapper'>
+          <h2>Hi There, I'm</h2>
+          <h3>Sandesh Raj Shakya</h3>
+          <h1>Computer Engineer <span ref={textRef} /></h1>
+        </div>
+        <a href="#portfolio">
+          <img src={arrow} alt="Arrow" />
+        </a>
       </div>
     </div>
   )
