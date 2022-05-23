a053b35f167835a95bba3a9b444a9e0e2c9b74bf
Sandesh Raj Shakya <sandsshakya444@gmail.com>
2022-05-23 10:49:14 +0545

    Changes to Intro

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,8 +1,17 @@
 import './Intro.scss'
+import profilePicture from "../../Images/img3.jpg"
 
 function Intro() {
   return (
     <div className='intro' id="intro">
+      <div className='left'>
+        <div className='imgContainer'>
+          <img src={profilePicture} alt="Profile Picutre" />
+        </div>
+      </div>
+      <div className='right'>
+
+      </div>
     </div>
   )
 }
