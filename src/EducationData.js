5b60b3e903779b8c7f4faeff067da34fd63732bc
Sandesh Raj Shakya <sandsshakya444@gmail.com>
2022-05-29 17:50:40 +0545

    Made changes images import in EducationData

--- src/EducationData.js
+++ src/EducationData.js
@@ -4,29 +4,23 @@ export const educationData = [
       title: "Kathmandu Engineering College(KEC)",
       location: "Kalimati, Kathmandu",
       info: "COLLEGE",
-      desc:
-        "I pursued my Bachelors degree in Computer Engineering from Kathmandu Engineering College.",
-      img:
-        "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+      desc: "I pursued my Bachelors degree in Computer Engineering from Kathmandu Engineering College.",
+      img: "https://raw.githubusercontent.com/Upsyco08/Portfolio/main/src/Images/kec.jpg",
     },
     {
       id: "2",
       title: "Prasadi Academy",
       location: "Manbhawan, Lalitpur",
       info: "HIGHER LEVEL EDUCATION",
-      desc:
-        "I pursued my +2 degree in Science faculty from Prasadi Academy.",
-      img:
-        "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+      desc: "I pursued my +2 degree in Science faculty from Prasadi Academy.",
+      img: "https://raw.githubusercontent.com/Upsyco08/Portfolio/main/src/Images/prasadi.png",
     },
     {
       id: "3",
       title: "GEMS",
       location: "Dhapakhel, Lalitpur",
       info: "LOWER LEVEL EDUCATION",
-      desc:
-        "I pursued my SLC degree from GEMS(Graded English Medium School.",
-      img:
-        "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
-    },
+      desc: "I pursued my SLC degree from GEMS(Graded English Medium School.",
+      img: "https://raw.githubusercontent.com/Upsyco08/Portfolio/main/src/Images/gems.jpg",
+      },
   ];
